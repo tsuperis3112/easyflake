6bcc2c03d3ee783a5bc2d3d2bc26871e3b25b9af
Takeru Furuse <tsuperis@gmail.com>
2023-02-26 01:48:27 +0900

    fix test

--- easyflake/grpc/server/exec.py
+++ easyflake/grpc/server/exec.py
@@ -1,12 +1,12 @@
 import os
 import signal
 import socket
+import sys
 from concurrent import futures
 from typing import Optional
 
 import click
 import grpc
-from daemon import DaemonContext
 from lockfile.pidlockfile import PIDLockFile
 
 from easyflake.grpc.protobuf import sequence_pb2_grpc
@@ -22,6 +22,17 @@ def serve(host: str, port: int, *, is_daemon=False, pid_file: Optional[str] = No
     endpoint = f"{host}:{port}"
 
     if is_daemon:
+        try:
+            from daemon import DaemonContext
+        except ImportError:
+            click.echo(
+                click.style(
+                    "The daemon feature is not supported by your system.",
+                    fg="red",
+                )
+            )
+            sys.exit(1)
+
         pid_path = pid_file and os.path.abspath(pid_file)
         pid_lock_file = PIDLockFile(pid_path) if pid_path else None
         with DaemonContext(pidfile=pid_lock_file):
